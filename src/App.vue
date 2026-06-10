65c21eeee142a857f1927f7477bc53220385b1e7
shenzhen <shenlongze@126.com>
2026-06-10 14:11:15 +0800

    feat: 全局开启微信小程序转发功能

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
 export default {
   onLaunch: function () {
     console.log('App Launch');
+    uni.showShareMenu({
+      withShareTicket: false,
+      menus: ['shareAppMessage']
+    });
   },
   onShow: function () {
     console.log('App Show');
@@ -9,6 +13,12 @@ export default {
   onHide: function () {
     console.log('App Hide');
   },
+  onShareAppMessage: function () {
+    return {
+      title: 'ScorePocket 台球记分助手',
+      path: '/pages/index/index'
+    };
+  }
 };
 </script>
 
